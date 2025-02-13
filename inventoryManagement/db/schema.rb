c6313b2d7cac03c6553ee37ca3ea3f5d8808fd30
NicholasMatias <n-r-matias@outlook.com>
2025-02-13 07:10:33 +0000

    install dotenv and setup env in gitignore => auth secret and id moved to env file

--- inventoryManagement/db/schema.rb
+++ inventoryManagement/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_11_230834) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_13_061057) do
+  create_table "admins", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
+
   create_table "categories", primary_key: "name", id: :string, force: :cascade do |t|
     t.string "parent_category"
     t.string "icon"
